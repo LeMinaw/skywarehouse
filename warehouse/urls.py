4eb57f9db64551a06f3433dad4c3719ed578fbe5
LeMinaw <leminaw@gmail.com>
2018-08-18 22:42:43 +0200

    urls dispatch rewrite for django 2.0

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'warehouse'
 urlpatterns = [
-    url(r'^$',                                               views.home,      name='home'),
-    url(r'^list/?$',                                         views.main,      name='main'),
-    url(r'^list/(?P<id>[0-9]+)/?$',                          views.main,      name='main'),
-    url(r'^list/(?P<slug>[-a-zA-Z0-9_]+)/?$',                views.main,      name='main'),
-    url(r'^list/(?P<slug>[-a-zA-Z0-9_]+)/(?P<id>[0-9]+)/?$', views.main,      name='main'),
-    url(r'^blueprint/new/?$',                                views.edit,      name='edit'),
-    url(r'^blueprint/(?P<slug>[-a-zA-Z0-9_]+)/edit/?$',      views.edit,      name='edit'),
-    url(r'^blueprint/(?P<slug>[-a-zA-Z0-9_]+)/?$',           views.blueprint, name='blueprint'),
-    url(r'^blueprint/(?P<slug>[-a-zA-Z0-9_]+)/files/?$',     views.files,     name='files'),
-    url(r'^user/(?P<username>[-a-zA-Z0-9_@.+]+)/?$',         views.user,      name='user'),
-    url(r'^user/(?P<username>[-a-zA-Z0-9_@.+]+)/edit/?$',    views.useredit,  name='useredit'),
+    path('',                           views.home,      name='home'),
+    path('list',                       views.main,      name='main'),
+    path('list/<int:id>',              views.main,      name='main'),
+    path('list/<str:slug>',            views.main,      name='main'),
+    path('list/<str:slug>/<int:id>',   views.main,      name='main'),
+    path('blueprint/new',              views.bp_edit,   name='blueprint_add'),
+    path('blueprint/<str:slug>/edit',  views.bp_edit,   name='blueprint_edit'),
+    path('blueprint/<str:slug>',       views.blueprint, name='blueprint'),
+    path('blueprint/<str:slug>/files', views.files,     name='files'),
+    path('user/<str:username>',        views.user,      name='user'),
+    path('user/<str:username>/edit',   views.user_edit, name='user_edit'),
 
-    url(r'^about/?$',                                        views.about,     name='about'),
+    path('about',                      views.about,     name='about'),
 ]
