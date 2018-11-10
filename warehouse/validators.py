f7da93bfc468eeadddb20f7e7e6d5cc230c5d360
LeMinaw <leminaw@gmail.com>
2018-11-10 15:15:28 +0100

    validators formating

--- warehouse/validators.py
+++ warehouse/validators.py
@@ -1,17 +1,18 @@
 from django.utils.deconstruct import deconstructible
 from django.core.exceptions   import ValidationError
 
+
 @deconstructible
 class MaxFileSizeValidator(object):
     message = 'Max allowed file size is %(max_size)s.'
     code = 'file_too_big'
 
     def __init__(self, max_size=10*1024**2, message=None, code=None):
-            self.max_size = max_size
-            if message is not None:
-                self.message = message
-            if code is not None:
-                self.code = code
+        self.max_size = max_size
+        if message is not None:
+            self.message = message
+        if code is not None:
+            self.code = code
 
     def __call__(self, value):
         if value.size > self.max_size:
