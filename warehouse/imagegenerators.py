6302fb0c6f27714f430cd8f3005e12a1ba6dbd44
LeMinaw <le@minaw.net>
2020-07-05 09:13:47 +0200

    fine tune thumbnailing settings

--- warehouse/imagegenerators.py
+++ warehouse/imagegenerators.py
@@ -3,8 +3,8 @@ from imagekit.processors import ResizeToFit
 
 
 class CardThumbnail(ImageSpec):
-    processors = [ResizeToFit(400, 400)]
+    processors = [ResizeToFit(400, 1000)]
     format = 'JPEG'
-    options = {'quality': 80}
+    options = {'quality': 90}
 
 register.generator('warehouse:card_thumbnail', CardThumbnail)
